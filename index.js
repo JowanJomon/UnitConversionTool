756c431b605112cab60b202c4320314b5bbfc8bd
Jowan Jomon <jowanjomon619@gmail.com>
2022-10-05 10:35:43 +0400

    Fixed the delete button

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const addbtn = document.getElementById("add");
 const savebtn = document.getElementById("save");
 const deletbtn = document.getElementById("delete");
-const link = document.getElementById("links");
+const links = document.getElementById("links");
 let leads = [];
 let storageleads = "";
 
+storageleads = JSON.parse(localStorage.getItem("leads"));
+if (storageleads) {
+  render(storageleads);
+}
+
 addbtn.addEventListener("click", add);
+deletbtn.addEventListener("click", clear);
 
 function add() {
   let link = document.getElementById("link").value;
   if (link) {
     leads.push(link);
-    render();
+    render(leads);
     pushLocalStoage();
   } else {
-    alert("Please Enter Something");
+    links.innerHTML = `<li>Please enter a valid link</li>`;
   }
 }
 
-function render() {
-  link.innerHTML = "";
-  for (i = 0; i < leads.length; i++) {
-    link.innerHTML += `<li>${leads[i]}</li>`;
+function render(x) {
+  links.innerHTML = "";
+  for (i = 0; i < x.length; i++) {
+    links.innerHTML += `<a href="${x[i]}" target="_blank"><li>${x[i]}</li><a>`;
   }
 }
 
 function pushLocalStoage() {
   storageleads = JSON.stringify(leads);
   localStorage.setItem("leads", storageleads);
+}
+
+function clear() {
+  localStorage.clear();
+  links.innerHTML = `<li>Please enter a link to store.</li>`;
 }
